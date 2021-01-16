135526b0de65368c990e7320a6828fe1fa9c6654
Blend Mehani <blendd_mehani@live.com>
2021-01-16 21:56:36 +0100

    Update settings.py

--- mydrive/settings.py
+++ mydrive/settings.py
@@ -159,6 +159,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'contact.mypersonaldrive@gmail.com'
-EMAIL_HOST_PASSWORD = 'obrphrhubktckhrj'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
